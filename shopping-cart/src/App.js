e026ad9fd44382c61d6d1ab341fed52f0463ff8c
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-11-10 00:00:44 +0330

    api link and product context provider added to my project

---
+++ shopping-cart/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ProductContextProvider from './context/ProductContextProvider';
+
+function App() {
+  return (
+    <ProductContextProvider >
+      App
+    </ProductContextProvider>
+  );
+}
+
+export default App;
---
+++ shopping-cart/src/context/ProductContextProvider.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react';
+
+// API:
+import { getProducts } from '../services/api';
+
+const ProductsContext = createContext();
+
+const ProductContextProvider = (props) => {
+
+    const[products,setProducts] = useState([])
+
+    useEffect(()=>{
+        const fetchAPI = async () =>{
+            setProducts(await getProducts())
+        }
+        fetchAPI();
+    },[])
+
+    return (
+        <ProductsContext.Provider value ={products}>
+            {props.children}
+        </ProductsContext.Provider>
+    );
+};
+
+export default ProductContextProvider;
