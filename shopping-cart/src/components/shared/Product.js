38e81577b01a2adfe891e67603eedc6ed95b82c3
mohamadnikparvar <88265337+mohamadnikparvar@users.noreply.github.com>
2021-11-14 12:13:02 +0330

    number of each products added

--- shopping-cart/src/components/shared/Product.js
+++ shopping-cart/src/components/shared/Product.js
@@ -28,6 +28,7 @@ const Product = ({productData}) => {
                 <div className={styles.buttonContainer}>
                     {quantityCount(state,productData.id) === 1 && <button className={styles.smallButton} onClick={()=>dispatch({type:"REMOVE_ITEM",payload:productData})}><img src={trashIcon} alt="trash"/></button>}
                     {quantityCount(state,productData.id) > 1 && <button className={styles.smallButton} onClick={()=>dispatch({type:"DECREASE",payload:productData})}>-</button>}
+                    {quantityCount(state,productData.id) > 0 && <span className={styles.counter}>{quantityCount(state,productData.id)}</span>}
                     {
                         isInCard(state,productData.id)?
                         <button className={styles.smallButton} onClick={()=>dispatch({type:"INCREASE",payload:productData})}>+</button>:
